efef4bd633e75ee6447064d72b35f420d9936b27
bonani2sardar <bonanisardar9748@gmail.com>
2023-07-15 19:25:55 +0530

    st

---
+++ stream_hello.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Set page title
+st.title("Hello, World!")
+
+# Display the message
+st.write("Welcome to Streamlit!")
+
+# Run the app
+if __name__ == "__main__":
+    st.run()
